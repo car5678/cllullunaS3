e80f3a2fb4d170137a7ebe7dad188334811acb60
master <master@SMLAB108PC20_23>
2023-06-13 21:22:16 -0500

    Agregar archivos de proyecto.

---
+++ cllullunaS3/cllullunaS3/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cllullunaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1( )
+        {
+            InitializeComponent();
+        }
+
+        private void btnabrir_Clicked(object sender, EventArgs e)
+        {
+            string  dato1 =Convert.ToString( txtdato1.Text);
+            double dato2 =Convert.ToDouble( txtdato2.Text);
+            Navigation.PushAsync(new Page2(dato1 , dato2)); 
+        }
+    }
+}
---
+++ cllullunaS3/cllullunaS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cllullunaS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1 , double dato2)
+        {
+            InitializeComponent();
+            lbldato1.Text = "dato uno " +dato1;
+            lbldato2.Text = "dato dos " + dato2.ToString();
+        }
+
+        private void btnAbriV3_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
